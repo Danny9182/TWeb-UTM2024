71e04d6ef64bb61a6e7ea3cf29383c2d994add2f
Cristina Stamatin <dear.volgk@gmail.com>
2024-03-18 11:34:47 +0200

    test

--- git-app-api/Domain/Entities/User/ULoginData.cs
+++ git-app-api/Domain/Entities/User/ULoginData.cs
@@ -5,11 +5,8 @@ namespace Domain.Entities.User
     public class ULoginData
     {
         public string Credentials { get; set; }
-        
         public string Password { get; set; }
-        
         public DateTime LoginDateTime { get; set; }
-        
         public string LoginIp { get; set; }
     }
 }
